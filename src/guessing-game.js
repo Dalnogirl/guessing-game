a6ce363b2f11565fe8351ebfb99e7c9254d26bd0
Hleb <velikiizevs@gmail.com>
2019-10-10 20:05:42 +0300

    add:  class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.array = [];
+        this.guessNumber;
+        this.minimal;
+        this.maximum;
+        this.result;
     }
 
+    setRange(min, max) {
+        for (let i = min; i <= max; i++) {
+            this.array.push(i);
+        }
+    }
     guess() {
-
+        this.guessNumber = this.array[Math.floor(this.array.length / 2)]
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.array = this.array.slice(0, this.array.indexOf(this.guessNumber) + 1);
     }
 
     greater() {
-
+        this.array = this.array.slice(this.array.indexOf(this.guessNumber), this.array.length);
     }
 }
 
